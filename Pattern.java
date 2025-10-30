505883f455a0a630f6408f5285c4d8f3cf467ba6
imredshadow <jianturalbaacol@gmail.com>
2025-10-30 18:27:35 +0000

    67

--- Pattern.java
+++ Pattern.java
@@ -1,1 +1,44 @@
+//Jian Acol
+//10/39/2025
+//brief description
+public class Pattern{
+    //precondition
+    //postcondition
+    public static void stars(int rows){
+        //code goes here
+        int totalRows = rows;
+        int currentRow = 1;
+        int numStars = 1;
+        while (currentRow < totalRows){
+            int i = 0;
+            while (i < numStars) {
+                System.out.print("*");
+                i++;
+            }
+            System.out.println("");
+            numStars += 2;
+            currentRow++;
+        }
+        
+    }
 
+    public static void triangle(int rows){
+        //code goes here
+        int totalRows = rows;
+        int currentRow = 1;
+        while (currentRow < totalRows){
+            int i = 0;
+            while (i < currentRow) {
+                System.out.print(currentRow);
+                i++;
+            }
+            System.out.println("");
+            currentRow++;
+        }
+        
+    }
+    public static void main(String[] args){
+        stars(7);
+        triangle(9);
+    }
+}
