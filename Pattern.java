226d2164b1f7756a520b0d20bc1a9d5be1989ce3
imredshadow <jianturalbaacol@gmail.com>
2025-11-06 19:05:29 +0000

    67

--- Pattern.java
+++ Pattern.java
@@ -2,14 +2,13 @@
 //10/39/2025
 //brief description
 public class Pattern{
-    //precondition
-    //postcondition
+    //precondition: Input a integer number of rows
+    //postcondition: Print the said amount of rows increasing by two each row
     public static void stars(int rows){
-        //code goes here
         int totalRows = rows;
         int currentRow = 1;
         int numStars = 1;
-        while (currentRow < totalRows){
+        while (currentRow <= totalRows){
             int i = 0;
             while (i < numStars) {
                 System.out.print("*");
@@ -21,9 +20,10 @@ public class Pattern{
         }
         
     }
-
+    
+    //precondition: Input a integer number of rows
+    //postcondition: Print the said amount of rows with the said number of that specified row as the number displayed in the row
     public static void triangle(int rows){
-        //code goes here
         int totalRows = rows;
         int currentRow = 1;
         while (currentRow < totalRows){
@@ -37,6 +37,8 @@ public class Pattern{
         }
     }
 
+    //precondition: Input a integer number of rows 
+    //postcondition: Print each odd numbered row specified in the input, with the specified row displayed as the number
     public static void odds(int start){
         for (int count = start; count >= 1; count -= 2) {
             for (int times = 0; times < count; times++) {
@@ -46,8 +48,10 @@ public class Pattern{
         }
     }
     
+    //precondition: Input a integer number of rows that it wont go past
+    //postcondition: Prints up to the specified number by 1, then decreases back down by 1,switching with O and E, however
+    //when inputted an odd number, will start with an E
     public static void eo(int maxE){
-        //code goes here
         String letter = "E";
         if (maxE%2 == 0)
             letter = "O";
@@ -77,8 +81,14 @@ public class Pattern{
         }
     }
 
+    //precondition: Input a integer number of rows
+    //postcondition: Print the said amount of rows with a odd number, decreasing by two each row, 
+    //and adding spaces to the beginning it appear in a pyramid shape, till the last row is one
     public static void pyramids(int rows){
-        for (int i = 1 ; i <= rows; i++) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j < i; j++){
+                System.out.print(" ");
+            }
             for (int count = 0; count < (rows - i) * 2 + 1; count++) {
                 System.out.print(i);
             }
